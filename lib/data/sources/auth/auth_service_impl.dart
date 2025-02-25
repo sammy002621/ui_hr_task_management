52b4cde16d2662cc2d69b70b52c49ab7f3cc0f6f
sammy002621 <kalindasammy4@gmail.com>
2025-02-25 12:06:59 +0200

    signup integration complete

--- lib/data/sources/auth/auth_service_impl.dart
+++ lib/data/sources/auth/auth_service_impl.dart
@@ -6,19 +6,17 @@ import 'package:http/http.dart' as http;
 class AuthServiceImpl implements AuthService {
   @override
   Future<Either> signIn() {
-    // TODO: implement signIn
+    // TODO: implement signIn -> priority
 
     throw UnimplementedError();
   }
 
 
-//TODO: today i should make an api that handles signup logic for the backend to test if my frontend is working
-
-// TODO: today i should make an api that handles signin logic for the backend  to finish the authentication related things 
+// TODO: try to find a way that the phone number will also be validated like the others as long as nothing is in it or it is empty
   @override
   Future<Either> signUp(CreateUser createUser) async {
     try {
-      var url = Uri.http('10.12.74.43:5000', '/api/register');
+      var url = Uri.http('10.0.2.2:5500', 'workmate/v1/auth/sign-up');
       var response = await http.post(url, body: {
         // we need to pass the body in
         'email': createUser.email,
@@ -28,13 +26,13 @@ class AuthServiceImpl implements AuthService {
       });
 
       // if the user was created successfully
-      if (response.statusCode == 200) {
+      if (response.statusCode == 200 || response.statusCode == 201) {
         // we will return a right with the user object
-        return Right(createUser);
+        return Right('USER CREATED SUCCESSFULLY_______________________ ${response.body}');
       } else {
         // if the user was not created successfully
         // we will return a left with the error message
-        return Left('Error creating user');
+        return Left('ERROR CREATING USER:_____________________________ ${response.body}');
       }
     } catch (e) {
       // if it fails we will return a left
